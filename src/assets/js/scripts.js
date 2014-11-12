78ca742c0a148fcbef31f1e350d8182cc5e1bfee
Mihail Yakimenko <macgera@gmail.com>
2014-11-13 00:50:05 +0100

    add posters in posts

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,11 +1,27 @@
 (function($) {
 
+    function screenSize(elem) {
+        var windowHeight = $(window).height();
+        $(elem).css('min-height', windowHeight);
+    }
+
+    // header in post
+    function headerInPost() {
+        var headerInPost = $('[data-role="in-post-poster"]');
+        var windowHeight = $(window).height();
+        headerInPost.css('bottom', windowHeight*.3);
+    }
+
     // document ready
     $(document).ready(function() {
+        screenSize('[data-role="post-poster"]');
+        headerInPost();
     });
 
     // all initial on window resize
     $(window).on('resize', function() {
+        screenSize('[data-role="post-poster"]');
+        headerInPost();
     });
 
 
